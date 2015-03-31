11244b1a8a32b66ed21801e61a825fda7dc6da5a
Eduardo Jesus Serna Leal <lalotahoma@icloud.com>
2015-03-31 17:44:10 -0600

    A;adi la clase Monstruo

---
+++ src/Monstruo.java
@@ -0,0 +1,23 @@
+/**
+ * JuegoApplet
+ *
+ * Esta clase es la base de monstruo, que incluye cosas basicas como nombre
+ * imagen, etc
+ *
+ * @author Eduardo Serna
+ * @version 1.0 2015/03/31
+ */
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.ImageObserver;
+import javax.swing.ImageIcon;
+
+public class Monstruo {
+    
+    private int iX;     //posicion en x.       
+    private int iY;     //posicion en y.
+    private ImageIcon imiIcono;	//icono.
+    private String sTipo;
+    
+}
