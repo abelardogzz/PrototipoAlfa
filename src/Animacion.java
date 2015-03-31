9facc07bd5fa5988dac16649cb501da6ec744f18
Eduardo Jesus Serna Leal <lalotahoma@icloud.com>
2015-03-31 16:24:11 -0600

    Comment added2.

--- src/Animacion.java
+++ src/Animacion.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 
 public class Animacion{
 		//SKJNDHJKDFHJKDKJHD	//SKJNDHJKDFHJKDKJHD	//SKJNDHJKDFHJKDKJHD	//SKJNDHJKDFHJKDKJHD	//SKJNDHJKDFHJKDKJHD	//SKJNDHJKDFHJKDKJHD
+    // jhgjhghjkjhgkghjghkj
 	private ArrayList cuadros;
 	private int indiceCuadroActual;
 	private long tiempoDeAnimacion;
