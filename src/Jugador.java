fb6d3281cf40e731c9d9c415e06822d7f3faf749
abelardogzz <abelardo_gonzalezg@hotmail.com>
2015-03-31 20:22:37 -0600

    Modifico clase Jugador

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,13 +1,178 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
+ * Jugador
  *
- * @author Abelardo
+ * Modela la definición de todos los objetos de tipo
+ * <code>Jugador</code>
+ *
+ * @author Abelardo Gzz, Eduardo S., Luis F.
+ * @version 1.0
+ * @date 31/Mar/15
  */
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.image.ImageObserver;
+import javax.swing.ImageIcon;
+
 public class Jugador {
     
+    private int iX;     //posicion en x.       
+    private int iY;     //posicion en y.
+    private int iAncho; //ancho del objeto
+    private int iAlto; //largo del objeto
+    private Image imaImagen;	//imagen.
+    
+    /**
+     * Jugador
+     * 
+     * Metodo constructor usado para crear el objeto animal
+     * creando el icono a partir de una imagen
+     * 
+     * @param iX es la <code>posicion en x</code> del objeto.
+     * @param iY es la <code>posicion en y</code> del objeto.
+     * @param iAncho es el <code>ancho</code> del objeto.
+     * @param iAlto es el <code>Largo</code> del objeto.
+     * @param imaImagen es la <code>imagen</code> del objeto.
+     * 
+     */
+    public Jugador(int iX, int iY , int iAncho, int iAlto,Image imaImagen) {
+        this.iX = iX;
+        this.iY = iY;
+        this.iAncho = iAncho;
+        this.iAlto = iAlto;
+        this.imaImagen = imaImagen;
+    }
+    
+    /**
+     * setX
+     * 
+     * Metodo modificador usado para cambiar la posicion en x del objeto
+     * 
+     * @param iX es la <code>posicion en x</code> del objeto.
+     * 
+     */
+    public void setX(int iX) {
+        this.iX = iX;
+    }
+
+    /**
+     * getX
+     * 
+     * Metodo de acceso que regresa la posicion en x del objeto 
+     * 
+     * @return iX es la <code>posicion en x</code> del objeto.
+     * 
+     */
+    public int getX() {
+            return iX;
+    }
+    
+    /**
+     * setY
+     * 
+     * Metodo modificador usado para cambiar la posicion en y del objeto 
+     * 
+     * @param iY es la <code>posicion en y</code> del objeto.
+     * 
+     */
+    public void setY(int iY) {
+            this.iY = iY;
+    }
+
+    /**
+     * getY
+     * 
+     * Metodo de acceso que regresa la posicion en y del objeto 
+     * 
+     * @return posY es la <code>posicion en y</code> del objeto.
+     * 
+     */
+    public int getY() {
+        return iY;
+    }
+    
+    /**
+     * setImagen
+     * 
+     * Metodo modificador usado para cambiar el icono de imagen del objeto
+     * tomandolo de un objeto imagen
+     * 
+     * @param imaImagen es la <code>imagen</code> del objeto.
+     * 
+     */
+    public void setImagen(Image imaImagen) {
+        this.imaImagen = imaImagen;
+    }
+
+    /**
+     * getImagen
+     * 
+     * Metodo de acceso que regresa la imagen que representa el icono del objeto
+     * 
+     * @return la imagen a partide del <code>icono</code> del objeto.
+     * 
+     */
+    public Image getImagen() {
+        return imaImagen;
+    }
+    
+    /**
+     * getAncho
+     * 
+     * Metodo de acceso que regresa el ancho del icono 
+     * 
+     * @return un <code>entero</code> que es el ancho de la imagen.
+     * 
+     */
+    public int getAncho() {
+        return iAncho;
+    }
+
+    /**
+     * getAlto
+     * 
+     * Metodo que  da el alto del icono 
+     * 
+     * @return un <code>entero</code> que es el alto de la imagen.
+     * 
+     */
+    public int getAlto() {
+        return iAlto;
+    }
+    
+    /**
+     * paint
+     * 
+     * Metodo para pintar el animal
+     * 
+     * @param graGrafico   objeto de la clase <code>Graphics</code> para graficar
+     * @param imoObserver  objeto de la clase <code>ImageObserver</code> es el 
+     *    Applet donde se pintara
+     * 
+     */
+    public void paint(Graphics graGrafico, ImageObserver imoObserver) {
+        graGrafico.drawImage(getImagen(), getX(), getY(), getAncho(), getAlto(), imoObserver);
+    }
+    
+    /**
+     * intersecta
+     *
+     * Metodo que checa si un animal intersecta a otro
+     *
+     * @param objObjeto es un objeto de la clase <code>Object</code>
+     * @return un boleano para saber si intersecta o no
+     */
+    public boolean intersecta(Object objObjeto) {
+        if (objObjeto instanceof Jugador) {
+            Rectangle rctEsteObjeto = new Rectangle(this.getX(), this.getY(),
+                    this.getAncho(), this.getAlto());
+            Jugador aniObjeto = (Jugador) objObjeto;
+            Rectangle rctObjetoParam = new Rectangle(aniObjeto.getX(),
+                    aniObjeto.getY(), aniObjeto.getAncho(), aniObjeto.getAlto());
+            return rctEsteObjeto.intersects(rctObjetoParam);
+        } 
+        else {
+            return false;
+        }
+    }
 }
